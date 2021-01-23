bac8fc33682d22a95ab148205aec2f7b94815932
gcxfd <i@rmw.link>
2021-01-23 18:24:24 +0800

    master

--- src/pkg/main/vue/_com/feed.vue
+++ src/pkg/main/vue/_com/feed.vue
@@ -1,55 +1,24 @@
 <style lang="stylus" scoped>
 main
+  display flex
+  flex-direction row
+  flex-wrap wrap
+  .ico
+    background 50% 50% / 8rem
+    margin 1rem
+    width 6rem
+    float left
+    box-shadow 0 0 0.3rem inset rgba(0, 0, 0, 0.5)
+    height @width
+    display inline-block
   form
     margin-top 2rem
 </style>
 
 <template lang="pug">
 main
-  h1 我问问 h1
-  a(href="/404") 404
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  h1 我问问 h1
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
-  a(href="/404") 404
+  .bk(v-for="i in li")
+    b.ico(:style="`background-image:url('//www.thiswaifudoesnotexist.net/example-${i}.jpg')`")
 
 </template>
 
@@ -61,7 +30,10 @@ components:{
 
 }
 setup:=>
-
+  li = [1..100000]
+  {
+    li
+  }
 }
 </script>
 
