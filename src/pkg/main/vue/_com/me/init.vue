2a622748b761e4a3bc42f01b826a9bc28659a9cc
gcxfd <i@rmw.link>
2021-01-30 21:47:00 +0800

    master

--- src/pkg/main/vue/_com/me/init.vue
+++ src/pkg/main/vue/_com/me/init.vue
@@ -1,5 +1,4 @@
 <style lang="stylus" scoped>
-@import '~@/styl/reset/form.styl'
 @import './me'
 main, footer
   margin-top 2.3rem
@@ -15,7 +14,6 @@ form
   padding 0 2.3rem
   &>h1
     margin-bottom 2rem
-  p
 form, footer
   background url("data:image/svg+xml;utf-8,<svg xmlns='http://www.w3.org/2000/svg' width='100%' height='1px'><line x1='0' y1='0' x2='100%' y2='0' stroke-dasharray='3,3' stroke='rgb(185,185,185)'></line></svg>") 0 0 no-repeat
 form, footer
@@ -28,39 +26,39 @@ form, footer
 
 <template lang="pug">
 scrollbar(ref="scroll")
-  main
+  main(ref="main")
     i.logo
     h2 去中心化 · 无服务器 · 人人平等 · 畅所欲言
-    form(ref="form" @submit.prevent="submit")
+    a-form(:model="form")
       h1 欢迎加入下一代互联网
-      p(:class="{s:data.lang}")
-        a-select(v-model:value="data.lang" :getPopupContainer="body" :showSearch="true")
+      a-form-item(:class="{s:form.lang}")
+        a-select(v-model:value="form.lang" :getPopupContainer="body" :showSearch="true")
           a-select-option(v-for="val in C.LANG" :value="val") {{val}}
         label 语言
-      p
-        input(placeholder=" ")
+      a-form-item
+        input(placeholder=" " required)
         label 姓名 或 昵称
-      p
-        input(placeholder=" ")
+      a-form-item
+        input(placeholder=" " required)
         label 公司 或 组织
-      p
-        input(placeholder=" ")
+      a-form-item
+        input(placeholder=" " required)
         label 职位 或 头衔
-      p(:class="{s:data.sex}")
-        a-select(v-model:value="data.sex" :getPopupContainer="body")
+      a-form-item(:class="{s:form.sex}")
+        a-select(v-model:value="form.sex" :getPopupContainer="body" required)
           a-select-option(v-for="val in C.SEX" :value="val") {{val}}
         label 性别 或 物种
-      p(:class="{s:data.zone}")
-        a-select(v-model:value="data.zone" :getPopupContainer="body" :showSearch="true")
+      a-form-item(:class="{s:form.zone}")
+        a-select(v-model:value="form.zone" :getPopupContainer="body" :showSearch="true")
           a-select-option(v-for="val in C.ZONE" :value="val") {{val}}
         label 国家 或 自治区
-      p
-        input(placeholder=" ")
+      a-form-item
+        input(placeholder=" " required)
         label 城市 或 城区
-      p
+      a-form-item
         input(placeholder=" ")
         label 头像
-      p
+      a-form-item
         button(type="submit") 创建账户
     footer
       a
@@ -73,6 +71,7 @@ scrollbar(ref="scroll")
 import Scrollbar from '@/lib/scrollbar'
 import {ref, shallowRef, onMounted} from 'vue'
 import ASelect from '@/lib/antd/select'
+import AForm from '@/lib/antd/form'
 import LANG from '@/const/lang'
 import ZONE from '@/const/zone'
 
@@ -80,40 +79,38 @@ export default {
 components:{
   Scrollbar
   ASelect
+  AForm
+  AFormItem:AForm.Item
   ASelectOption:ASelect.Option
 }
 setup:=>
   scroll = shallowRef()
-  form = shallowRef()
-
+  main = shallowRef()
   {language} = navigator
   language_prefix = language.split("-",1)[0]
   for [en,name] from LANG
     if [language,language_prefix].indexOf(en)+1
       lang = name
       break
-  data = ref {
+  form = ref {
     lang
     zone:""
     sex:""
   }
   onMounted =>
-    form.value.getElementsByTagName("input")[1].focus()
+    # form.value.getElementsByTagName("input")[1].focus()
     return
   {
+    main
     form
     scroll
-    submit:=>
-      console.log data.value
-      return
     body:=>
-      form.value
+      main.value
     C:{
       LANG:(i[0] for i from LANG)
       ZONE:(i[0] for i from ZONE)
       SEX:"男/女/机器人".split('/')
     }
-    data
   }
 }
 </script>
